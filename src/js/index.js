042d1c47d2c2180b7d1fa8642d54e4b8d71a8f4c
Nick <thedeadofblackfire@yahoo.com>
2017-08-28 21:23:53 +0200

    init

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+import config from './config';
+import Game from './objects/Game';
+
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        if (config.isCordova) {
+			console.log('cordova ok2');
+            document.addEventListener("deviceready", this.deviceReady, false);
+        } else {
+            document.addEventListener("DOMContentLoaded", this.desktopReady, false);
+        }
+    },
+
+    deviceReady: function() {
+        app.deviceSetup();
+        app.gameStart();
+    },
+
+    desktopReady: function(){
+        app.gameStart();
+    },
+
+    deviceSetup: function(){
+        window.screen.lockOrientation('landscape');
+    },
+
+    gameStart: function() {
+        config.init();
+        window.game = new Game();
+        window.config = config;
+    }
+};
+
+app.initialize();
